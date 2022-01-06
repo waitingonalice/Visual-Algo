283d42b11a49239cef29bacb08a299be348f60a4
Wilson Sie <fcsie55@gmail.com>
2022-01-06 23:58:51 +0800

    fixed some bugs in quicksort

--- src/SortingAlgorithms/quickSortAlgo.js
+++ src/SortingAlgorithms/quickSortAlgo.js
@@ -21,11 +21,13 @@ const partition = (low, high, array, animations) => {
     let i = low, pivot = array[high], j = low;
     //at each iteration of j,
     //swap j with i if j < pivot value, separating values smaller and larger than pivot
+    animations.push([i, j])
+    animations.push([i, j])
     for (j; j < array[high - 1]; j++) {
         if (array[j] < pivot) {
             animations.push([i, array[j]]);
-            animations.push([i, array[j]]);
-            animations.push([j, array[i]]);
+            // animations.push([i, array[j]]);
+            // animations.push([j, array[i]]);
             animations.push([j, array[i]]);
             let temp = array[i];
             array[i] = array[j];
@@ -36,8 +38,8 @@ const partition = (low, high, array, animations) => {
 
     //main partition -- once j reaches array[high -1], swap pivot to value[i];
     animations.push([i, array[high]]);
-    animations.push([i, array[high]]);
-    animations.push([high, array[i]]);
+    // animations.push([i, array[high]]);
+    // animations.push([high, array[i]]);
     animations.push([high, array[i]]);
     let temp = array[i];
     array[i] = array[high];
